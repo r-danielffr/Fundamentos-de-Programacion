f58e5bf6df04b4f4bc626731c6cf228bcd8537c7
r-danielffr <00246325@uca.edu.sv>
2025-05-20 12:56:55 -0600

    Add files via upload

---
+++ C.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int i,num;
+    
+    cout<<"Ingrese un numero: ";
+    cin>>num;
+    cout<<" "<<endl;
+
+    for(i=num; i<=10; i++){
+        cout<<i<<endl;}
+
+return 0;}
